2ee61e403fc3f07ce6d88b181d1489f8a4ea3480
akashsachithanandham <46894142+akashsachithanandham@users.noreply.github.com>
2021-02-12 20:20:15 +0530

    integrated sentry

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from "./App";
+
+Sentry.init({
+  dsn: "https://2a572cb1209d4bb9ada7c366d6ac79fa@o522811.ingest.sentry.io/5634705",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
